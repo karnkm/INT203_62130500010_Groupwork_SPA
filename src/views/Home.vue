64e1a7b851919cb1ac04554b6f86672031a7f3e4
Karnkamol Phattanaphirome <kmkarn1213@gmail.com>
2021-04-10 14:20:20 +0700

    [update] show form db

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
     <h2>home</h2>
+    <div v-for="student in students" :key="student.id">
+      <p>{{student.firstname}} {{student.lastname}} {{student.age}}</p>
+    </div>
   </div>
 </template>
 
@@ -10,5 +13,16 @@
 export default {
   name: "Home",
   components: {},
+  data() {
+    return {
+      students:[]
+    }
+  },
+
+  async mounted() {
+    const response = await fetch("http://localhost:3000/students")
+    this.students = await response.json()
+  },
+  
 };
 </script>
