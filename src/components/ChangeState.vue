60a185ffd84afdbd640ac0d15d1710bfca5a7396
Karnkamol Phattanaphirome <kmkarn1213@gmail.com>
2021-04-18 00:08:23 +0700

    62130500010[update] style page about

--- src/components/ChangeState.vue
+++ src/components/ChangeState.vue
@@ -1,55 +1,55 @@
 <template>
-<p class="text-xl italic font-semibold text-center pt-8">Edit your data and don't forget to checking.</p>
+<p class="text-xl italic font-semibold text-center pt-8">Edit your data and don't forget for checking.</p>
 <div class="text-center">
 <form @submit.prevent>
       <div class="grid grid-cols-2 gap-4 mt-5 p-12 bg-pink-100">
                 <div class="flex flex-col float-right">
                     <label for="firstname" class="text-sm leading-7 text-gray-600">First Name</label>
-                    <input type="text" id="firstname" v-model="name" name="firstname" class="bg-white border-b-2 border-blue-300 rounded px-4 py-2 mb-4">
+                    <input type="text" id="firstname" v-model="name" name="firstname" class="bg-white border-b-2 border-pink-300 rounded px-4 py-2 mb-4">
                 </div>
                 <div class="flex flex-col">
                     <label for="lastname" class="text-sm leading-7 text-gray-600">Last Name</label>
-                    <input type="text" id="lastname" v-model="lastname" name="lastname" class="bg-white border-b-2 border-blue-300  rounded px-4 py-2 mb-4">
+                    <input type="text" id="lastname" v-model="lastname" name="lastname" class="bg-white border-b-2 border-pink-300  rounded px-4 py-2 mb-4">
                 </div>
                 <div class="flex flex-col">
                     <label for="age" class="text-sm leading-7 text-gray-600">Age</label>
-                    <input type="number" id="age" v-model="age" name="age" class="bg-white border-b-2 border-blue-300  rounded px-4 py-2 mb-4">
+                    <input type="number" id="age" v-model="age" name="age" class="bg-white border-b-2 border-pink-300  rounded px-4 py-2 mb-4">
                 </div>
                 <div class="flex flex-col">
                     <label class="text-sm leading-7 text-gray-600">Gender</label>
                     <div class=" pt-2 space-x-2 align-middle">
-                        <input type="radio" id="Male" v-model="gender" name="gender" value="M" class="bg-white border-b-2 border-blue-300  rounded px-4 py-2 mb-4 ">
+                        <input type="radio" id="Male" v-model="gender" name="gender" value="M" class="bg-white border-b-2 border-pink-300  rounded px-4 py-2 mb-4 ">
                         <label for="Male">Male</label>
-                        <input type="radio" id="Female" v-model="gender" name="gender" value="F" class="bg-white border-b-2 border-blue-300  rounded px-4 py-2 mb-4">
+                        <input type="radio" id="Female" v-model="gender" name="gender" value="F" class="bg-white border-b-2 border-pink-300  rounded px-4 py-2 mb-4">
                         <label for="Female">Female</label>
                     </div>
                 </div>
                 <div class="flex flex-col">
                     <label for="email" class="text-sm leading-7 text-gray-600">Email</label>
-                    <input type="text" id="email" v-model="email" name="email" class="bg-white border-b-2 border-blue-300  rounded px-4 py-2 mb-4">
+                    <input type="text" id="email" v-model="email" name="email" class="bg-white border-b-2 border-pink-300  rounded px-4 py-2 mb-4">
                 </div>
                 <div class="flex flex-col">
                     <label for="phone" class="text-sm leading-7 text-gray-600">Phone Number</label>
-                    <input type="text" id="phone" v-model="phone" name="phone" class="bg-white border-b-2 border-blue-300  rounded px-4 py-2 mb-4">
+                    <input type="text" id="phone" v-model="phone" name="phone" class="bg-white border-b-2 border-pink-300  rounded px-4 py-2 mb-4">
                 </div>
                 <div class="flex flex-col">
                     <label class="text-sm leading-7 text-gray-600">Select Vaccine</label>
                     <div class=" pt-2 space-x-2 align-middle">
-                        <input type="radio" id="pfizer" v-model="vaccine" name="vaccine" value="pfizer" class="bg-white border-b-2 border-blue-300  rounded px-4 py-2 mb-4 ">
+                        <input type="radio" id="pfizer" v-model="vaccine" name="vaccine" value="pfizer" class="bg-white border-b-2 border-pink-300  rounded px-4 py-2 mb-4 ">
                         <label for="pfizer">Pfizer-BioNTech</label>
-                        <input type="radio" id="jj" v-model="vaccine" name="vaccine" value="jj" class="bg-white border-b-2 border-blue-300  rounded px-4 py-2 mb-4 ">
+                        <input type="radio" id="jj" v-model="vaccine" name="vaccine" value="jj" class="bg-white border-b-2 border-pink-300  rounded px-4 py-2 mb-4 ">
                         <label for="jj">Johnson&Johnsonh</label>
-                        <input type="radio" id="astra" v-model="vaccine" name="vaccine" value="astra" class="bg-white border-b-2 border-blue-300  rounded px-4 py-2 mb-4 ">
+                        <input type="radio" id="astra" v-model="vaccine" name="vaccine" value="astra" class="bg-white border-b-2 border-pink-300  rounded px-4 py-2 mb-4 ">
                         <label for="astra">AstraZeneca</label>
-                        <input type="radio" id="moderna" v-model="vaccine" name="vaccine" value="moderna" class="bg-white border-b-2 border-blue-300  rounded px-4 py-2 mb-4 ">
+                        <input type="radio" id="moderna" v-model="vaccine" name="vaccine" value="moderna" class="bg-white border-b-2 border-pink-300  rounded px-4 py-2 mb-4 ">
                         <label for="moderna">Moderna</label>
-                        <input type="radio" id="sinovac" v-model="vaccine" name="vaccine" value="sinovac" class="bg-white border-b-2 border-blue-300  rounded px-4 py-2 mb-4 ">
+                        <input type="radio" id="sinovac" v-model="vaccine" name="vaccine" value="sinovac" class="bg-white border-b-2 border-pink-300  rounded px-4 py-2 mb-4 ">
                         <label for="sinovac">Sinovac</label>
                     </div>
                 </div>
                 <div class="flex flex-col">
                     <label for="allergy" class="text-sm leading-7 text-gray-600">Drug allergy</label>
-                    <input type="text" id="allergy" v-model="allergy" name="allergy" class="bg-white border-b-2 border-blue-300  rounded px-4 py-2 mb-4">
+                    <input type="text" id="allergy" v-model="allergy" name="allergy" class="bg-white border-b-2 border-pink-300  rounded px-4 py-2 mb-4">
                 </div>
 
             </div>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,85 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <h1 class="text-xl italic font-semibold text-center pt-8">✧ More Vaccine for you ✦</h1>
+
+    <div class="grid mt-5 bg-yellow-100 rounded-2xl mx-12">
+      <div class="">
+        <img src="../assets/vaccine/pfizer_b.png" class="w-40 mb-12 mt-12 ml-12 mr-12 float-left">
+        <span class="inline float-right mt-10 mb-8 mr-28 space-y-6 text-lg font-normal"> 
+        <p>Name : Pfizer-BioNTech</p>
+        <p>Owner Country : <img src="https://thaiphotos.net/download/UHVBeWt3TTdweFpYSXdNWWpldTdQZz09" class="rounded-3xl h-11 ">+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Flag_of_Germany_%283-2_aspect_ratio%29.svg/1280px-Flag_of_Germany_%283-2_aspect_ratio%29.svg.png" class="rounded-full h-14"></p>
+        <p>Technology : The new mRNA technology tricks the body into making the viral protein itself which, in turn, triggers an immune response. </p>
+        <p>Efficientcy : 62-90%</p>
+        <p>Process : Passed all three trials </p>
+        <p>Dose Required : 2 </p>
+      </span>
+      </div>
+    </div>
+
+    <div class="grid mt-5 bg-yellow-100 rounded-2xl mx-12">
+      <div class="">
+        <img src="../assets/vaccine/jj_b.png" class="w-40 mb-12 mt-12 ml-12 mr-12 float-left">
+        <span class="inline float-right mt-10 mr-28 space-y-6 text-lg font-normal"> 
+        <p>Name : Johnson&Johnson</p>
+        <p>Owner Country : <img src="https://thaiphotos.net/download/UHVBeWt3TTdweFpYSXdNWWpldTdQZz09" class="rounded-3xl h-11 "></p>
+        <p>Technology : Uses a Covid virus to deliver genetic material from the coronavirus into the body to prompt an immune response. </p>
+        <p>Efficientcy : CDC and FDA have recommended a pause in the use of the Janssen (Johnson & Johnson) COVID-19 vaccine in the United States</p>
+        <p>Process : Passed all three trials </p>
+        <p>Dose Required : 1 </p>
+      </span>
+      </div>
+    </div>
+
+    <div class="grid mt-5 bg-yellow-100 rounded-2xl mx-12">
+      <div class="">
+        <img src="../assets/vaccine/moderna_b.png" class="w-40 mb-12 mt-12 ml-12 float-left">
+        <span class="inline float-right mt-10 mr-28 space-y-6 text-lg font-normal"> 
+        <p>Name : Moderna</p>
+        <p>Owner Country : <img src="https://thaiphotos.net/download/UHVBeWt3TTdweFpYSXdNWWpldTdQZz09" class="rounded-xl h-12 "></p>
+        <p>Technology : A new type of vaccine which uses messenger RNA, which contains instructions for human cells <br>
+        to make proteins that mimic part of the coronavirus, to trigger an immune response. </p>
+        <p>Efficientcy : 95%</p>
+        <p>Process : Passed all three trials </p>
+        <p>Dose Required : 2 </p>
+      </span>
+      </div>
+    </div>
+
+    <div class="grid mt-5 bg-yellow-100 rounded-2xl mx-12">
+      <div class="">
+        <img src="../assets/vaccine/astra_b.png" class="w-40 mb-12 mt-12 ml-12 float-left">
+        <span class="inline float-right mt-8 mb-8 mr-28 space-y-6 text-lg font-normal"> 
+          <p>Name : AstraZeneca</p>
+          <p class="">Owner Country : <img src="https://quinl.quinlcdn.com/productImages/UploadImages/2103/1616996675_67182.png" class="rounded-3xl h-11 ">+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Flag_of_Sweden.svg/240px-Flag_of_Sweden.svg.png" class="rounded-3xl h-11 "></p>
+          <p>Technology : Viral Vector(Genetically Modified virus) When injected, the vaccine instructs human cells to produce <br>
+            the SARS-CoV-2 spike protein - the immune system's main target in Coronaviruses. </p>
+          <p>Efficientcy : 62-90%</p>
+          <p>Process : Passed all three trials </p>
+          <p>Dose Required : 2 </p>
+        </span>
+      </div>
+    </div>
+
+    <div class="grid mt-5 bg-yellow-100 rounded-2xl mx-12">
+      <div class="">
+        <img src="../assets/vaccine/sinovac_b.png" class="w-40 mb-12 mt-12 ml-12 float-left">
+        <span class="inline float-right mt-10 mr-28 space-y-6 text-lg font-normal"> 
+          <p>Name : Sinovac</p>
+          <p>Owner Country : <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Flag_of_the_People%27s_Republic_of_China.svg/225px-Flag_of_the_People%27s_Republic_of_China.svg.png" class="rounded-3xl h-11 "></p>
+          <p>Technology : Inactive vaccine, using the dead Covid-19 virus itself to trigger an immune response. </p>
+          <p>Efficientcy : 50-70%</p>
+          <p>Process : Passed three trials </p>
+          <p>Dose Required : 2 </p>
+        </span>
+      </div>
+    </div>
+
+    
+  </div>  
 </template>
+
+<script>
+export default {
+    
+};
+</script>
